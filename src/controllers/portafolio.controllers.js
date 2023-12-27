03c030f96004daa8955ae9c86692afa5e7486b75
vinculando_intellij <git@github.com:SandovalBrandon1027/vinculando_intellij.git>
2023-12-27 14:05:12 -0500

    imagenes en cloudinary

--- src/controllers/portafolio.controllers.js
+++ src/controllers/portafolio.controllers.js
@@ -1,5 +1,6 @@
 const Portfolio = require('../models/Portfolio')
-const { uploadImage } = require('../config/cloudinary')
+const fs = require('fs-extra')
+const { uploadImage,deleteImage } = require('../config/cloudinary')
 
 // METODO PARA LISTAR LOS PORTAFOLIOS
 const renderAllPortafolios = async(req,res)=>{
@@ -35,33 +36,58 @@ const renderAllPortafolios = async(req,res)=>{
 
     //MEtodo para guardar el formulario en BDD
     const updatePortafolio = async(req,res)=>{
-        const {title,category,description}= req.body
-        await Portfolio.findByIdAndUpdate(req.params.id,{title,category,description})
+        const portfolio = await Portfolio.findById(req.params.id).lean()
+        if(portfolio._id != req.params.id) return res.redirect('/portafolios')
+        
+        if(req.files?.image) {
+            if(!(req.files?.image)) return res.send("Se requiere una imagen")
+            await deleteImage(portfolio.image.public_id)
+            const imageUpload = await uploadImage(req.files.image.tempFilePath)
+            const data ={
+                title:req.body.title || portfolio.name,
+                category: req.body.category || portfolio.category,
+                description:req.body.description || portfolio.description,
+                image : {
+                public_id:imageUpload.public_id,
+                secure_url:imageUpload.secure_url
+                }
+            }
+            await fs.unlink(req.files.image.tempFilePath)
+            await Portfolio.findByIdAndUpdate(req.params.id,data)
+        }
+        else{
+            const {title,category,description}= req.body
+            await Portfolio.findByIdAndUpdate(req.params.id,{title,category,description})
+        }
         res.redirect('/portafolios')
     }
 
     
     // METODO PARA ELIMINAR EN LA BASE DE DATOS
+
     const deletePortafolio = async(req,res)=>{
-        await Portfolio.findByIdAndDelete(req.params.id)
+        const portafolio = await Portfolio.findByIdAndDelete(req.params.id)
+        await deleteImage(portafolio.image.public_id)
         res.redirect('/portafolios')
     }
-    
     
     
     const renderPortafolioForm = (req,res)=>{
     res.render('portafolio/newFormPortafolio')
     }
     
-  
     const createNewPortafolio =async (req,res)=>{
 
         const {title, category,description} = req.body   
         const newPortfolio = new Portfolio({title,category,description})
         newPortfolio.user = req.user._id
-        
         if(!(req.files?.image)) return res.send("Se requiere una imagen")
-        await uploadImage(req.files.image.tempFilePath)
+        const imageUpload = await uploadImage(req.files.image.tempFilePath)
+        newPortfolio.image = {
+            public_id:imageUpload.public_id,
+            secure_url:imageUpload.secure_url
+        }
+        await fs.unlink(req.files.image.tempFilePath)
         await newPortfolio.save()
         res.redirect('/portafolios')
     }
